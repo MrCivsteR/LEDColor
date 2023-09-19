79f78b2d1b9e2b126e8c997d18571fa253fe3ba4
MrCivsteR <141365588+MrCivsteR@users.noreply.github.com>
2023-09-19 18:07:27 -0400

    Initial Commit
    Initial Commit

---
+++ LEDControl/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using OpenLibSys;
+using System.Windows.Controls;
+using System.Windows;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LEDControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                ReadColorInfo();
+            }
+            else
+            {
+                VerifyArgs(args);
+            }
+        }
+        public static void VerifyArgs(string[] args)
+        {
+            if (!uint.TryParse(args[0], out uint joystick)) return;
+            if (1 < joystick && joystick > 3) return;
+
+            uint red = 0;
+            uint green = 0;
+            uint blue = 0;
+
+            if (args.Length > 1) red = Clamp(args[1], 0, 255);
+            if (args.Length > 2) green = Clamp(args[2], 0, 255);
+            if (args.Length > 3) blue = Clamp(args[3], 0, 255);          
+
+            uint[] color = {red, green, blue};
+
+            SetJoyStick(joystick, color);
+        }
+        public static void ReadColorInfo()
+        {
+            string appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string filePath = Path.Combine(appDataFolder, "LEDColor\\ColorInfo.txt");
+
+            if (!File.Exists(filePath)) return;
+
+            var lines = File.ReadLines(filePath);
+
+            foreach (var line in lines)
+            {
+                string[] args = line.Split(' ');
+                VerifyArgs(args);
+            }
+        }
+        public static void SetJoyStick(uint js, uint[] color)
+        {
+            Worker worker = new Worker();       
+            worker.SetPixel(js, 1, color);
+            worker.SetPixel(js, 2, color);
+            worker.SetPixel(js, 3, color);
+            worker.SetPixel(js, 4, color);
+        }
+        public static uint Clamp(string arg, uint min, uint max)
+        {
+            if (uint.TryParse(arg, out uint value))
+            {
+                return value < min ? min : value > max ? max : value;
+            }
+            else
+            {
+                return 0;
+            }
+            
+        }
+    }
+}
---
+++ LEDControl/Worker.cs
@@ -0,0 +1,97 @@
+﻿using OpenLibSys;
+using System;
+using System.Data.OleDb;
+using System.Threading;
+
+namespace LEDControl
+{
+    public class Worker
+    {
+        const uint EC_OBF = 0x01;  // Output Buffer Full
+        const uint EC_IBF = 0x02;  // Input Buffer Full
+        const uint EC_DATA = 0x62; // Data Port
+        const uint EC_SC = 0x66;   // Status/Command Port
+        const uint RD_EC = 0x80;   // Read Embedded Controller
+        const uint WR_EC = 0x81;   // Write Embedded Controller
+
+        private readonly Ols ols = new Ols();
+
+        public void SetPixel(uint js, uint position, uint[] color)
+        {
+            if (ols.GetStatus() != (uint)Ols.Status.NO_ERROR) return;
+            if (ols.GetDllStatus() != (uint)Ols.OlsDllStatus.OLS_DLL_NO_ERROR) return;
+            
+            ECCommand(js, position * 3, color[0]);
+            ECCommand(js, position * 3 + 1, color[1]);
+            ECCommand(js, position * 3 + 2, color[2]);
+        }
+
+        private void ECCommand(uint command, uint parameter1, uint parameter2)
+        {
+            ECRAMWrite(0x6d, command);
+            ECRAMWrite(0xb1, parameter1);
+            ECRAMWrite(0xb2, parameter2);
+            ECRAMWrite(0xbf, 0x10);
+            Thread.Sleep(10);
+            ECRAMWrite(0xbf, 0xff);
+            Thread.Sleep(10);
+        }
+
+        public void ECRAMDone()
+        {
+            ECRAMWrite(0xbf, 0x10);
+            Thread.Sleep(10);
+            ECRAMWrite(0xbf, 0xff);
+            Thread.Sleep(10);
+        }
+
+        private void ECRAMWrite(uint address, uint data)
+        {
+            SendECCommand(WR_EC);
+            SendECData(address);
+            SendECData(data);
+        }
+
+        private void SendECCommand(uint command)
+        {
+            if (ECReady())
+            {
+                OlsWrite(EC_SC, command);
+            }
+        }
+
+        private void SendECData(uint data)
+        {
+            if (ECReady())
+            {
+                OlsWrite(EC_DATA, data);
+            }
+        }
+
+        private bool ECReady()
+        {
+            var timeout = DateTime.Now.Add(TimeSpan.FromMilliseconds(50));
+            while (DateTime.Now < timeout && (OlsRead(EC_SC) & EC_IBF) != 0x0)
+            {
+                Thread.Sleep(1);
+            }
+
+            if (DateTime.Now <= timeout)
+            {
+                return true;
+            }
+            Console.WriteLine("EC not ready!");
+            return false;
+        }
+
+        private void OlsWrite(uint port, uint value)
+        {
+            ols.WriteIoPortByte((ushort)port, (byte)value);
+        }
+
+        private uint OlsRead(uint port)
+        {
+            return ols.ReadIoPortByte((ushort)port);
+        }
+    }
+}
